5b23d8ceb79dfc96d460a76b88edef468fb181ed
kaikeffranca <141879723+kaikeffranca@users.noreply.github.com>
2024-09-16 12:14:24 -0300

    Add files via upload

---
+++ Atividade6/ativ6a/ativ6a.js
@@ -0,0 +1,7 @@
+var nome = prompt("Digite o nome do aluno: ");
+var n1 = parseFloat(prompt("Digite a primeira nota do aluno: "))
+var n2 = parseFloat(prompt("Digite a segunda nota do aluno: "))
+var n3 = parseFloat(prompt("Digite a terceira nota do aluno: "))
+var n4 = parseFloat(prompt("Digite a quarta nota do aluno: "))
+
+alert(((n1+n2+n3+n4) / 4).toFixed(2));
---
+++ Atividade6/ativ6b/ativ6b.js
@@ -0,0 +1,8 @@
+var n1 = parseFloat(prompt("Digite o primeiro número: "));
+var n2 = parseFloat(prompt("Digite o segundo número: "));
+
+alert("Soma: " + (n1+n2).toFixed(2));
+alert("Subtração: "+ (n1-n2).toFixed(2));
+alert("Multiplicação: "+ (n1*n2).toFixed(2));
+alert("Divisão: "+ (n1/n2).toFixed(2));
+alert("Resto da divisão: "+ (n1%n2).toFixed(2));
